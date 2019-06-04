e43ebc3d130d44001c9d605229f24950d2439036
Nacho Fanjul <nfanjul@plainconcepts.com>
2019-06-04 18:05:14 +0200

    images and data

---
+++ appworkshop/src/Api/data.js
@@ -0,0 +1,14 @@
+import Person0 from '../images/0.png';
+
+const defaultData = {
+  'name': 'María',
+  'eyeColor': 'brown',
+  'hairColor': 'Black',
+  'age': 0,
+  'languages': [],
+  'work': 'student',
+  'salary': 0,
+  'picture': Person0,
+};
+
+export default defaultData;
