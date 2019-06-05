87a79887293ecf18dd2086743bcda2763abd03b0
Nacho Fanjul <nfanjul@plainconcepts.com>
2019-06-05 11:54:28 +0200

    sub menu2

--- appworkshop/src/components/children/Menu.jsx
+++ appworkshop/src/components/children/Menu.jsx
@@ -9,6 +9,11 @@ class Menu extends React.Component {
         <div>Option 1</div>
         <div>Option 2</div>
         <div>Option 3</div>
+        <div>sus sub <code>Example</code> with different children:</div>
+        <SubMenu>
+          <div>Option 1.1</div>
+          <div>Option 1.2</div>
+        </SubMenu>
       </SubMenu>
       <div>Second <code>Example</code> with different children:</div>
       <SubMenu>
